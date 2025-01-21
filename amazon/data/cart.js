36d3beb3f0589905ba1a227a29d48b8df4dafc1b
cjtimola <christianjuliustimola@gmail.com>
2025-01-21 23:16:59 +0800

    Create automated tests for addToCart

--- amazon/data/cart.js
+++ amazon/data/cart.js
@@ -1,15 +1,21 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }
-];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage () {
+  cart = JSON.parse(localStorage.getItem('cart')) || [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];  
+}
 
 export function saveToStorage () {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -25,11 +31,11 @@ export function addToCart (productId, quantity) {
   });
 
   if (matchingItem) {
-    matchingItem.quantity += Number(quantity);
+    matchingItem.quantity += Number(quantity) || 1;
   } else {
     cart.push({
       productId,
-      quantity: Number(quantity),
+      quantity: Number(quantity) || 1,
       deliveryOptionId: '1'
     });
   }
