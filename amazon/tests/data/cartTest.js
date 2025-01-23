7fdfb77788c6586fa1c101603195364eb231c343
cjtimola <christianjuliustimola@gmail.com>
2025-01-23 14:59:27 +0800

    Check if the set values in the localStorage are correct

--- amazon/tests/data/cartTest.js
+++ amazon/tests/data/cartTest.js
@@ -1,9 +1,11 @@
 import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
-  it('adds an existing product to the cart', () => {
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
+  });
 
+  it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([
         {
@@ -20,11 +22,10 @@ describe('test suite: addToCart', () => {
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify(cart));
   });
 
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem');
-
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     });
@@ -35,5 +36,6 @@ describe('test suite: addToCart', () => {
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify(cart));
   });
 });
