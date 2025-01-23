79c8ce9c3a2abee8c40d1ac307627cb6307da15b
cjtimola <christianjuliustimola@gmail.com>
2025-01-23 19:33:50 +0800

    Move test for updating delivery option to the renderOrderSummary suite

--- amazon/tests/checkout/orderSummaryTest.js
+++ amazon/tests/checkout/orderSummaryTest.js
@@ -76,40 +76,6 @@ describe('test suite: renderOrderSummary', () => {
       document.querySelector(`.js-product-name-${productId2}`).innerHTML.trim()
     ).toEqual('Intermediate Size Basketball');
   });
-});
-
-describe('test suite: updateDeliveryOption', () => {
-  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-
-  beforeEach(() => {
-    spyOn(localStorage, 'setItem');
-
-    document.querySelector('.js-test-container').innerHTML = `
-      <div class="js-checkout-header-middle-section"></div>
-      <div class="js-order-summary"></div>
-      <div class="js-payment-summary"></div>
-    `;
-
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([{
-        productId: productId1,
-        quantity: 2,
-        deliveryOptionId: '1'
-      }, {
-        productId: productId2,
-        quantity: 1,
-        deliveryOptionId: '2'
-      }]);
-    });
-    loadFromStorage();
-
-    renderOrderSummary();  
-  });
-
-  afterEach(() => {
-    document.querySelector('.js-test-container').innerHTML = '';
-  });
 
   it('updates delivery option', () => {
     document.querySelector(`.js-delivery-option-${productId1}-3`).click();
