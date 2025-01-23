ce2d1bc975fc91266196c2739a80d9b00d0d9752
cjtimola <christianjuliustimola@gmail.com>
2025-01-23 19:29:40 +0800

    Create test for updating the delivery option

--- amazon/scripts/checkout/paymentSummary.js
+++ amazon/scripts/checkout/paymentSummary.js
@@ -41,7 +41,7 @@ export function renderPaymentSummary () {
 
     <div class="payment-summary-row">
       <div>Shipping &amp; handling:</div>
-      <div class="payment-summary-money">
+      <div class="payment-summary-money js-shipping-price">
         $${formatCurrency(shippingPriceCents)}
       </div>
     </div>
@@ -62,7 +62,7 @@ export function renderPaymentSummary () {
 
     <div class="payment-summary-row total-row">
       <div>Order total:</div>
-      <div class="payment-summary-money">
+      <div class="payment-summary-money js-total-price">
         $${formatCurrency(totalCents)}
         </div>
     </div>
--- amazon/tests/checkout/orderSummaryTest.js
+++ amazon/tests/checkout/orderSummaryTest.js
@@ -1,5 +1,5 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
-import {loadFromStorage, cart} from '../../data/cart.js';
+import {loadFromStorage, cart, updateDeliveryOption} from '../../data/cart.js';
 
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
@@ -75,5 +75,50 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerHTML.trim()
     ).toEqual('Intermediate Size Basketball');
+  });
+});
+
+describe('test suite: updateDeliveryOption', () => {
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+
+    document.querySelector('.js-test-container').innerHTML = `
+      <div class="js-checkout-header-middle-section"></div>
+      <div class="js-order-summary"></div>
+      <div class="js-payment-summary"></div>
+    `;
+
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]);
+    });
+    loadFromStorage();
+
+    renderOrderSummary();  
+  });
+
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
+  it('updates delivery option', () => {
+    document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+    expect(document.querySelector(`.delivery-option-input-${productId1}-3`).checked)
+      .toEqual(true);
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+    expect(document.querySelector('.js-shipping-price').innerHTML.trim()).toEqual('$14.98');
+    expect(document.querySelector('.js-total-price').innerHTML.trim()).toEqual('$63.50');
   });
 });
