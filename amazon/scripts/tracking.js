7a1d75323caeda3da4eaebd67a3369f01e5d5434
cjtimola <christianjuliustimola@gmail.com>
2025-02-04 15:33:37 +0800

    Set delivery progress bar and correct delivery status

--- amazon/scripts/tracking.js
+++ amazon/scripts/tracking.js
@@ -17,7 +17,7 @@ loadPage();
 
 function renderTrackingSummary () {
   document.querySelector('.js-cart-quantity')
-  .innerHTML = cart.countCartQuantity();
+    .innerHTML = cart.countCartQuantity();
 
   const url = new URL(window.location.href);
   const orderId = url.searchParams.get('orderId');
@@ -37,6 +37,18 @@ function renderTrackingSummary () {
   
   const estimatedDeliveryTime = orderProduct.estimatedDeliveryTime;
   
+  const percentProgress = ((new Date() - new Date(matchingOrder.orderTime)) / (new Date(estimatedDeliveryTime) - new Date(matchingOrder.orderTime))) * 100;
+
+  let deliveryStatus = '';
+
+  if (percentProgress >= 0 && percentProgress <= 49) {
+    deliveryStatus = 'preparing';
+  } else if (percentProgress >= 50 && percentProgress <= 99) {
+    deliveryStatus = 'shipped';
+  } else if (percentProgress >= 100) {
+    deliveryStatus = 'delivered';
+  }
+
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
@@ -57,23 +69,26 @@ function renderTrackingSummary () {
     <img class="product-image" src="${matchingProduct.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label js-progress-label-preparing">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label js-progress-label-shipped">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label js-progress-label-delivered">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentProgress}%;"></div>
     </div>
   `;
 
   document.querySelector('.js-order-tracking')
     .innerHTML = trackingHTML;
+
+  document.querySelector(`.js-progress-label-${deliveryStatus}`)
+    .classList.add('current-status');
 
 }
